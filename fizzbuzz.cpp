87f51c0c9341f183b9535870c47750416b2caf6a
noahwgriffith <griffnoahw@gmail.com>
2020-05-01 19:05:08 -0400

    init

---
+++ fizzbuzz.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+  for(int i = 1; i <= 100; i++) {
+    std::string output = "";
+    if(i % 3 == 0) { output += "Fizz"; }
+    if(i % 5 == 0) { output += "Buzz"; }
+    if(output == "") {
+      output += std::to_string(i);
+    }
+    std::cout << output << std::endl;
+  }
+  return 0;
+}
